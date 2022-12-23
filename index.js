5c8069bacc96ccb0f4c40985d35e69d0d9b5c315
VIkasKaushikk <vikaskaushikk@yahoo.com>
2022-12-23 13:12:08 +0530

    Updated port

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const url = require('url');
  const data =  fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
  const tempOverview =  fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
  const tempProduct =  fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
+  const homePage =  fs.readFileSync(`${__dirname}/HTML/Main/index.html`,'utf-8');
  const tempCard =  fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
  const dataObj = JSON.parse(data);
  const replaceTemplate = (temp,product) => {
@@ -50,7 +51,8 @@ const server = http.createServer((req,res) => {
         const outputHtml = tempOverview.replace('{%productCards%}',cardHtml);
         res.end(outputHtml);        
     }else if(pathName === '/' || pathName === '/product'){
-        res.end('<h1>index page of application</h1>');        
+         res.writeHead(200,{'content-type':'text/html'});
+        res.end(homePage);        
     }else if(pathName === '/api'){            
         res.writeHead(200,{'content-type':'application/json'});
         res.end(data);
@@ -62,7 +64,7 @@ const server = http.createServer((req,res) => {
         res.end('<h1>Page Not Found!</h1>'); 
     }
 });
-server.listen(8000,'127.0.0.1',()=>{
-console.log('Listeing to request on port 8000');
+server.listen(3000,()=>{
+console.log('Listeing to request on port 3000');
 });
 
