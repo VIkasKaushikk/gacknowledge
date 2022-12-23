39770826b679f91c6cb253e757bd48f3c01489b4
VIkasKaushikk <vikaskaushikk@yahoo.com>
2022-12-23 13:26:12 +0530

    Updated port

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ const url = require('url');
  const data =  fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
  const tempOverview =  fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
  const tempProduct =  fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
-  const homePage =  fs.readFileSync(`${__dirname}/HTML/Main/index.html`,'utf-8');
  const tempCard =  fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
  const dataObj = JSON.parse(data);
  const replaceTemplate = (temp,product) => {
